8a592af19a91009a8d1f4ca6bb6a47afc4e200d6
swayamp876 <swayamp876@gmail.com>
2026-05-28 20:32:55 +0530

    Add files via upload

---
+++ simplecalculator.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2, result;
+
+    cout << "=== Simple Calculator ===" << endl;
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            cout << num1 << " + " << num2 << " = " << result << endl;
+            break;
+
+        case '-':
+            result = num1 - num2;
+            cout << num1 << " - " << num2 << " = " << result << endl;
+            break;
+
+        case '*':
+            result = num1 * num2;
+            cout << num1 << " * " << num2 << " = " << result << endl;
+            break;
+
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << num1 << " / " << num2 << " = " << result << endl;
+            } else {
+                cout << "Error: Division by zero!" << endl;
+            }
+            break;
+
+        default:
+            cout << "Error: Invalid operator!" << endl;
+    }
+
+    return 0;
+}
